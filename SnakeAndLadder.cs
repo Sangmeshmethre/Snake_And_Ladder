82767bbc4b352495cbbfdf2a5ec53923634a053d
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 21:11:42 +0530

    InitialPosistion

---
+++ SnakeAndLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderUC1
+{
+    public  class SnakeAndLadder
+    {
+        public static void InitialPosition()
+        {
+            int Position1=0;
+            Console.WriteLine("Initial Position Of Player 1 is:" + Position1);
+        }
+    }
+}
