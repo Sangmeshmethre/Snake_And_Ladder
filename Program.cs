168d31f0da161a89052987e61ba02bd54ff23f77
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 21:30:02 +0530

    Extar Winning Position

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderUC5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.ExactWinningPostion();
+
+        }
+    }
+}
