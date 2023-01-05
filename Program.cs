73fb95e6f6226345ac8ed92637f7c9272501290d
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 21:21:03 +0530

    Check Opation

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderUC3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.CheckPosition();
+        }
+    }
+}
