d7d74fa4afecca1e4c642fba3c8700ce09f5e235
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 21:25:52 +0530

    Winning Position

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderUC4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeAndLadder obj = new SnakeAndLadder();
+            obj.WinningPostion();
+        }
+    }
+}
